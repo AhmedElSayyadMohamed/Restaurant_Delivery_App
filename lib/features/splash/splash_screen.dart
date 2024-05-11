06b8b8bf5aff66f49759006cfbb3209a0a73e555
AhmedElSayyadMohamed <elsayyada197@gmail.com>
2024-05-11 23:44:33 +0300

    app setup

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -6,7 +6,11 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(child: Text('Splash Screen')),
+      body: Stack(
+        children: [
+          // Image.asset(''),
+        ],
+      ),
     );
   }
 }
